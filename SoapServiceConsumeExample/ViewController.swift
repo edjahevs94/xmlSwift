cac7d609f2af133cba5e1172c02e6f1da2b9bc3c
Edgard <20133252@ealoe.ulima.edu.pe>
2022-09-29 16:25:15 -0500

    Initial Commit

---
+++ SoapServiceConsumeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoapServiceConsumeExample
+//
+//  Created by EdgardVS on 29/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
