979b53308437223a7d93740485b9c76e492da08d
Edgard <20133252@ealoe.ulima.edu.pe>
2022-10-06 17:37:54 -0500

    simplificando codigo

--- SoapServiceConsumeExample/SoapRequest.swift
+++ SoapServiceConsumeExample/SoapRequest.swift
@@ -12,44 +12,17 @@ import XMLMapper
 struct Service {
     
     static func getStringData(parameters:[String:Any] = [:], completion: @escaping (Swift.Result<String, Error>) -> ()) {
-        let headers: HTTPHeaders = [
-         .authorization(username: "", password: "")]
-      
-        let soapMessage = HolidayServiceMessage(soapAction: "web:CapitalCity")
+    
+        let soapMessage = ServiceMessage(soapAction: "web:CapitalCity")
         soapMessage.countryCode = "US"
         let soapEnvelope = SOAPEnvelope(soapMessage: soapMessage, soapVersion: .version1point0)
         
-        print(soapEnvelope.toXMLString() ?? "nil")
-        
-        let service = Service(
-            name: "dataservice/transaction.getView",
-            parameters: [
-                Parameter(
-                    name: "columnList",
-                    value: "PERSON.FIRSTNAME,PERSON.PREFIX,PERSON.NAME,ACCESSKEY.RCN,READER.NAME"
-                ),
-                Parameter(
-                    name: "filterTransTypeList",
-                    value: "1,2"
-                ),
-                Parameter(
-                    name: "fromDate",
-                    value: "2021-01-17 00:00:00"
-                ),
-            ]
-        )
-        
-        //print(service.toXMLString() ?? "nil")
-        let service2 = Service2(country: Country(value: "US"))
-        //print(service2.toXMLString() ?? "nil")
-        let message2 = SOAPMessage(XMLString: service2.toXMLString() ?? "nil")
-        let envelope3 = SOAPEnvelope(soapMessage: message2!)
-        //print(envelope3.toXMLString() ?? "nil")
+        //print(soapEnvelope.toXMLString() ?? "nil")
         
         AF.request("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso", method: .post, parameters: soapEnvelope.toXML(), encoding: XMLEncoding.default).responseString { response in
             switch response.result {
-            case .success(let token):
-             completion(.success(token))
+            case .success(let data):
+             completion(.success(data))
             case .failure(let error):
              completion(.failure(error))
             }
@@ -57,84 +30,7 @@ struct Service {
         
         }
 
-    
-    class Service: XMLMappable {
-        var nodeName: String! = "SERVICE"
-        
-        var name: String?
-        var parameters: [Parameter]?
-        
-        init(name: String?, parameters: [Parameter]?) {
-            self.name = name
-            self.parameters = parameters
-        }
-        
-        required init?(map: XMLMap) {}
-        
-        func mapping(map: XMLMap) {
-            name <- map.attributes["name"]
-            parameters <- map["PARAMETERS.PARAMETER"]
-        }
-    }
-
-    class Parameter: XMLMappable {
-        var nodeName: String!
-        
-        var name: String?
-        var value: String?
-        
-        init(name: String?, value: String?) {
-            self.name = name
-            self.value = value
-        }
-        
-        required init?(map: XMLMap) {}
-        
-        func mapping(map: XMLMap) {
-            name <- map.attributes["name"]
-            value <- map.innerText
-        }
-    }
-    
-    class Service2: XMLMappable {
-        var nodeName: String! = "web:CapitalCity"
-        
-        var country: Country?
-        
-        init(country: Country?) {
-            self.country = country
-        }
-        
-        required init?(map: XMLMap) {
-            
-        }
-        
-        func mapping(map: XMLMap) {
-            country <- map["web:sCountryISOCode"]
-        }
-    }
-    
-    class Country: XMLMappable {
-        
-        var nodeName: String!
-        var value: String?
-        
-        init(value: String?) {
-            self.value = value
-        }
-        
-        required init?(map: XMLMap) {
-            
-        }
-        
-        func mapping(map: XMLMap) {
-            value <- map.innerText
-        }
-        
-        
-    }
-    
-    class HolidayServiceMessage: SOAPMessage {
+    class ServiceMessage: SOAPMessage {
 
         var countryCode: String?
 
